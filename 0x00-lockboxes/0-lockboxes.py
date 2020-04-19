374648c59be041d6e1b15da76896a0b22eb183b3
Gareth Brickman <977@holbertonschool.com>
2020-04-19 20:39:52 +0000

    [ADD] Some partially useful algos

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -1,7 +1,25 @@
 #!/usr/bin/python3
-from collections import deque
+# from collections import deque
 
 
 def canUnlockAll(boxes):
     """Unlock array of boxes of keys with indices"""
-    pass
+    n_boxes = len(boxes) - 1
+    unique_key_list = []
+
+    if not boxes[0]:
+        return False
+
+    for box in boxes:
+        for key in box:
+            if key not in unique_key_list and key != 0:
+                unique_key_list.append(key)
+
+    n_key_list = len(unique_key_list)
+
+    if n_key_list != n_boxes:
+        return False
+
+    if (n_boxes/2*(1+n_boxes)) == (n_key_list/2*(1+n_key_list)):
+        return True
+    return False
