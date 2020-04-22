1b58deb856bcffc592e3837236618cdf2d385316
Gareth Brickman <977@holbertonschool.com>
2020-04-22 19:42:39 +0000

    [INIT] Files

---
+++ 0x01-insert_in_sorted_linked_list/0-insert_number.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+#include "linked_lists.c"
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+/**
+ * insert_node - insets a number into a sorted listint_t list
+ * @head: pointer to pointer of first node of listint_t list
+ * @number: number to be included in list
+ * Return: the address of the new node, or NULL if it failed
+ */
+listint_t *insert_node(listint_t **head, int number)
+{
+
+}
