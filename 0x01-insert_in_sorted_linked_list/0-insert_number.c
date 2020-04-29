6ec17ab6e756121fde4e70b6ef8c0ab4cd8936e8
Gareth Brickman <977@holbertonschool.com>
2020-04-29 00:29:42 +0000

    [FIX] Working functions

--- 0x01-insert_in_sorted_linked_list/0-insert_number.c
+++ 0x01-insert_in_sorted_linked_list/0-insert_number.c
@@ -1,8 +1,6 @@
 #include "lists.h"
-#include "linked_lists.c"
-#include <stdlib.h>
-#include <string.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * insert_node - insets a number into a sorted listint_t list
@@ -12,5 +10,39 @@
  */
 listint_t *insert_node(listint_t **head, int number)
 {
+	listint_t *current;
+	listint_t *new;
 
+	/* NULL head check, error */
+	if (!head)
+		return (NULL);
+
+	/* empty list check, insert new node automatically*/
+	if (!(*head))
+		return (add_nodeint_end(head, number));
+
+	/* copy head to be able to move through list */
+	current = *head;
+
+	/* search for where n > number */
+	while (current->next)
+	{
+		if (current->next->n >= number)
+		{
+			/* insert new node into this position */
+			new = malloc(sizeof(listint_t));
+			if (!new)
+				return (NULL);
+			new->n = number;
+			new->next = current->next;
+			current->next = new;
+			return (new);
+		}
+		/* end of list reached */
+		else if (!current->next->next)
+			return (add_nodeint_end(head, number));
+		current = current->next;
+	}
+	/* workaround for non-void function error */
+	return (NULL);
 }
