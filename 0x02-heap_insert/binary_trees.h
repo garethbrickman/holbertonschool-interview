6727a6a9814f7bc0c191f6d9d283e412f170dd3f
Gareth Brickman <977@holbertonschool.com>
2020-05-05 01:42:27 +0000

    [FIX] Incl typedef for heap_t

--- 0x02-heap_insert/binary_trees.h
+++ 0x02-heap_insert/binary_trees.h
@@ -19,8 +19,10 @@ typedef struct binary_tree_s
 	struct binary_tree_s *right;
 } binary_tree_t;
 
+typedef struct binary_tree_s heap_t;
+
 void binary_tree_print(const binary_tree_t *);
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
-/* heap_t *heap_insert(heap_t **root, int value); */
+heap_t *heap_insert(heap_t **root, int value);
 
 #endif /* _BINARY_TREES_H_ */
