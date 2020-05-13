26488600b8a89dddf672dbac31a1f9e32cd14afe
Gareth Brickman <977@holbertonschool.com>
2020-05-13 19:40:35 +0000

    [INIT] Files

---
+++ 0x04-sandpiles/0-sandpiles.c
@@ -0,0 +1,13 @@
+#include "sandpiles.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * sandpiles_sum - function that computes the sum of two sandpiles
+ * @grid1: stable sandpile
+ * @grid2: stable sandpile
+ * Return: grid1 in stable condition
+ */
+void sandpiles_sum(int grid1[3][3], int grid2[3][3])
+{
+}
---
+++ 0x04-sandpiles/sandpiles.h
@@ -0,0 +1,6 @@
+#ifndef _SANDPILES_H_
+#define _SANDPILES_H_
+
+void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
+
+#endif /* _SANDPILES_H_ */
