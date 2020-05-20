ffd4229dbd63cc32bca8127ff5fa63788e88859a
Gareth Brickman <977@holbertonschool.com>
2020-05-20 00:38:13 +0000

    [FIX] Get 0-sandpiles to print grids

--- 0x04-sandpiles/0-sandpiles.c
+++ 0x04-sandpiles/0-sandpiles.c
@@ -6,8 +6,36 @@
  * sandpiles_sum - function that computes the sum of two sandpiles
  * @grid1: stable sandpile
  * @grid2: stable sandpile
- * Return: grid1 in stable condition
+ * Return: void
  */
 void sandpiles_sum(int grid1[3][3], int grid2[3][3])
 {
+	if (!grid1)
+	stable_print_grid(grid1);
+
+	if (!grid2)
+		stable_print_grid(grid2);
+}
+
+/**
+ * stable_print_grid - function that prints sandpile grid
+ * @grid: input sandpile
+  * Return: void
+ */
+void stable_print_grid(int grid[3][3])
+{
+	int i, j;
+
+	printf("=\n");
+
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 3; j++)
+		{
+			if (j)
+				printf(" ");
+			printf("%d", grid[i][j]);
+		}
+		printf("\n");
+	}
 }
--- 0x04-sandpiles/sandpiles.h
+++ 0x04-sandpiles/sandpiles.h
@@ -2,5 +2,6 @@
 #define _SANDPILES_H_
 
 void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
+void stable_print_grid(int grid[3][3]);
 
 #endif /* _SANDPILES_H_ */
