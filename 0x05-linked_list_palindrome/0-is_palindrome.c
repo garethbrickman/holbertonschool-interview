00f58ebba5d4dd0507c65b0c2ad98736f1ec2a3e
Gareth Brickman <977@holbertonschool.com>
2020-05-28 16:26:30 +0000

    Add required files

---
+++ 0x05-linked_list_palindrome/0-is_palindrome.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * is_palindrome - checks if singly linked list in palindrome
+ * @head: ptr to ptr to head of list
+ * Return: 0 if not a palindrome, 1 if a palindrome
+ */
+int is_palindrome(listint_t **head)
+{
+	if (!*(head))
+		return (1);
+	return (0);
+}
