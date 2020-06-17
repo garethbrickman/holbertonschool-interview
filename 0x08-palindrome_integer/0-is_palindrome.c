b09e583686395bff16a25983e87dc51af40d9ca0
Gareth Brickman <977@holbertonschool.com>
2020-06-17 00:08:57 +0000

    Init files

---
+++ 0x08-palindrome_integer/0-is_palindrome.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "palindrome.h"
+
+/**
+ * is_palindrome - checks if unsigned int is palindrome
+ * @n: unsigned int of given number to be checked
+ * Return: 1 if palindrome, 0 if not
+ */
+int is_palindrome(unsigned long n)
+{
+	if (!n)
+		return (0);
+	return (1);
+}
---
+++ 0x08-palindrome_integer/palindrome.h
@@ -0,0 +1,8 @@
+#ifndef PALINDROME_H
+#define PALINDROME_H
+
+#include <stdlib.h>
+
+int is_palindrome(unsigned long n);
+
+#endif /* PALINDROME_H */
