bd9ea1b3543e2fde9500ab0d213de1e5b269dcc6
Gareth Brickman <977@holbertonschool.com>
2020-06-25 02:57:49 +0000

    Add bit masking methods to check composition of bytes for UTF-8 validation

--- 0x09-utf8_validation/0-validate_utf8.py
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -3,7 +3,34 @@
 
 
 def validUTF8(data):
-    if not data:
+    # checks if most significant byte is 1
+    mask1 = 1 << 7
+    # checks if second most significant byte is 0
+    mask2 = 1 << 6
+    # keeps track of how many 1s before 0 occurs
+    n_bytes = 0
+
+    if not data or len(data) == 0:
+        return True
+
+    for num in data:
+        mask = 1 << 7
+        if n_bytes == 0:
+            while (mask & num):
+                n_bytes += 1
+                mask = mask >> 1
+
+            if n_bytes == 0:
+                continue
+            if n_bytes == 1 or n_bytes > 4:
+                return False
+        else:
+
+            if not (num & mask1 and not (num & mask2)):
+                return False
+        n_bytes -= 1
+
+    if n_bytes:
         return False
     else:
         return True
