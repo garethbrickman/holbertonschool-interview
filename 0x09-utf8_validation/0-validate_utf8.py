b953f46952000c46311b13fbf4eb0b37ccc4658e
Gareth Brickman <977@holbertonschool.com>
2020-06-25 02:38:13 +0000

    Init files

---
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" determines if a given data set represents a valid UTF-8 encoding """
+
+
+def validUTF8(data):
+    if not data:
+        return False
+    else:
+        return True
---
+++ 0x09-utf8_validation/README.md
@@ -0,0 +1,1 @@
+This is a README for my 0x09-utf8_validation project!
