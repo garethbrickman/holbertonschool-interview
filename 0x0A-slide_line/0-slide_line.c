5a53c58fefe3a120590a15550875cc0b1dedd2d7
Gareth Brickman <977@holbertonschool.com>
2020-06-30 22:39:55 +0000

    Init commit

---
+++ 0x0A-slide_line/0-slide_line.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "slide_line.h"
+
+
+/**
+ * slide_line - slides and merges an array of integers
+ * @line: ptr array of ints containing size elements
+ * @size: size of array
+ * @direction: int direction elements must be slid and merged to
+ * Return: 1 on success, 0 on failure
+ */
+int slide_line(int *line, size_t size, int direction)
+{
+	if (!line || !size || !direction)
+		return (0);
+	return (1);
+
+}
---
+++ 0x0A-slide_line/slide_line.h
@@ -0,0 +1,11 @@
+#ifndef SLIDE_LINE_H
+#define SLIDE_LINE_H
+
+#include <stdlib.h>
+
+#define SLIDE_LEFT 32
+#define SLIDE_RIGHT 32
+
+int slide_line(int *line, size_t size, int direction);
+
+#endif /* SLIDE_LINE_H */
