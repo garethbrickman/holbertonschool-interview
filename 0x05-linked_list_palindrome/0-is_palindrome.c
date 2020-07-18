422ec69e9e6a112615ed5cf7f77ea015076d5fce
Gareth Brickman <977@holbertonschool.com>
2020-07-18 17:59:01 +0000

    Fix betty

--- 0x05-linked_list_palindrome/0-is_palindrome.c
+++ 0x05-linked_list_palindrome/0-is_palindrome.c
@@ -9,7 +9,39 @@
  */
 int is_palindrome(listint_t **head)
 {
-	if (!*(head))
+	listint_t *copy = *head, *end = *head, *prev, *hold;
+	int size, i, is_pali = 1;
+
+	if (!head)
+		return (0);
+	/* get size */
+	for (size = 0; copy; size++)
+		copy = copy->next;
+	if (!*head || size == 1)
 		return (1);
-	return (0);
+	copy = *head;
+	/* Flip half */
+	for (i = 0; i < size; i++)
+	{
+		hold = end->next;
+		if (i > size / 2)
+			end->next = prev;
+		prev = end;
+		if (hold)
+			end = hold;
+	}
+	prev = NULL;
+	/* Check if palindrome and flip half back*/
+	for (i = 0; i < size / 2; i++)
+	{
+		if (copy->n != end->n)
+			is_pali = 0;
+		hold = end->next;
+		end->next = prev;
+		prev = end;
+		end = hold;
+
+		copy = copy->next;
+	}
+	return (is_pali);
 }
