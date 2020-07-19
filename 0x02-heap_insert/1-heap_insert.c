f6178da91b8312ce5b5ea909d768007ea8762815
Gareth Brickman <977@holbertonschool.com>
2020-07-19 22:37:34 +0000

    Add complete solution

--- 0x02-heap_insert/1-heap_insert.c
+++ 0x02-heap_insert/1-heap_insert.c
@@ -4,45 +4,115 @@
 
 
 /**
- * count_nodes - counts the number of nodes inside the tree.
- * @tree: binary tree node.
- * Return: number of nodes in the tree.
+ * find_insert_node - finds the node to insert the child in
+ * @root: the root of the tree
+ * @height: the current node height
+ * @maxheight: the max tree height
+ * Return: the node to enter the child in
  */
-int count_nodes(heap_t *tree)
+binary_tree_t *find_insert_node(binary_tree_t *root, int height, int maxheight)
 {
-	if (!tree)
-		return (0);
-	return (1 + count_nodes(tree->left) + count_nodes(tree->right));
-}
+	binary_tree_t *x;
+	binary_tree_t *y;
 
-/**
- * is_complete - function that checks if tree is complete.
- * @tree: binary tree node.
- * @index: index of the tree.
- * @n: number of nodes.
- * Return: 1 on success 0 on failure.
- */
-heap_t *is_complete(heap_t *tree, int index, int n)
-{
-	heap_t *left, *right;
-
-	if ((tree->left) && !(tree->right))
-		return (tree);
-	if (!(tree->left) && !(tree->right))
+	if ((root->left) && !(root->right))
+		return (root);
+	if (!(root->left || root->right))
 	{
-		if (index == n)
+		if (height == maxheight)
 			return (NULL);
 		else
-			return (tree);
+			return (root);
 	}
 
-	left = is_complete(tree->left, (2 * index) + 1, n);
-	right = is_complete(tree->right, (2 * index) + 2, n);
-
-	if  (left)
-		return (left);
+	x = find_insert_node(root->left, height + 1, maxheight);
+	y = find_insert_node(root->right, height + 1, maxheight);
+	if (x)
+		return (x);
 	else
-		return (right);
+		return (y);
+}
+
+
+/**
+ * change_attributes - swaps all three attributes on two nodes.
+ * @node1: the first node
+ * @node2: the second node
+ */
+void change_attributes(binary_tree_t *node1, binary_tree_t *node2)
+{
+	binary_tree_t *temp;
+
+	temp = node2->parent;
+	node2->parent = node1->parent;
+	node1->parent = temp;
+
+	temp = node2->left;
+	node2->left = node1->left;
+	node1->left = temp;
+
+	temp = node2->right;
+	node2->right = node1->right;
+	node1->right = temp;
+}
+
+
+/**
+ * change - change an item with its parent
+ * @item: the item
+ * @parent: the parent
+ */
+void change(binary_tree_t *item, binary_tree_t *parent)
+{
+	change_attributes(item, parent);
+	parent->parent = item;
+	if (item->left == item)
+	{
+		item->left = parent;
+		if (item->right)
+			item->right->parent = item;
+	}
+	if (item->right == item)
+	{
+		item->right = parent;
+		if (item->left)
+			item->left->parent = item;
+	}
+
+	if (item->parent)
+	{
+		if (item->parent->left == parent)
+			item->parent->left = item;
+		if (item->parent->right == parent)
+			item->parent->right = item;
+	}
+
+	if (parent->left)
+		if (parent->left->parent == item)
+			parent->left->parent = parent;
+
+	if (parent->right)
+		if (parent->right->parent == item)
+			parent->right->parent = parent;
+}
+
+
+
+/**
+ * max_heapify - turn heap into heap
+ * @x: the current bubble-up node
+ * Return: the root of the heapified tree
+ */
+heap_t *max_heapify(binary_tree_t *x)
+{
+	binary_tree_t *y;
+
+	y = x;
+	while (y->parent && y->n > y->parent->n)
+		change(y, y->parent);
+	while (y->parent)
+		y = y->parent;
+	return ((heap_t *)y);
 }
 
 /**
@@ -53,37 +123,39 @@ heap_t *is_complete(heap_t *tree, int index, int n)
  */
 heap_t *heap_insert(heap_t **root, int value)
 {
-	heap_t *new;
-	heap_t *parent;
-	int size;
+	binary_tree_t *x;
+	binary_tree_t *to_insert;
+	binary_tree_t *temp;
+	int h = 0;
 
-	new = malloc(sizeof(heap_t));
-
-	if (!new)
+	if (!root)
 		return (NULL);
-	parent = malloc(sizeof(heap_t));
-	new->parent = NULL;
-	new->left = NULL;
-	new->right = NULL;
-	new->n = value;
-
-	if (!*root)
+	x = binary_tree_node(NULL, value);
+	if (x == NULL)
+		return (NULL);
+	if (!(*root))
 	{
-		*root = new;
-		return (new);
+		*root = x;
+		return (x);
 	}
-
-	size = count_nodes(*root);
-	parent = is_complete(*root, 0, size);
-	if (!parent->left)
+	temp = *root;
+	while (temp->left)
 	{
-		parent->left = new;
-		new->parent = parent;
+		temp = temp->left;
+		h = h + 1;
 	}
+	to_insert = find_insert_node(*root, 0, h);
+	if (!to_insert)
+	{
+		to_insert = *root;
+		while (to_insert->left)
+			to_insert = to_insert->left;
+	}
+	if (to_insert->left)
+		to_insert->right = x;
 	else
-	{
-		parent->right = new;
-		new->parent = parent;
-	}
-	return (new);
+		to_insert->left = x;
+	x->parent = to_insert;
+	*root = max_heapify(x);
+	return (x);
 }
