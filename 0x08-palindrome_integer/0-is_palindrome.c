b2d7be4e4c1be1d0088b7d1ddb306340d204919a
Gareth Brickman <977@holbertonschool.com>
2020-07-19 19:19:08 +0000

    Complete solution

--- 0x08-palindrome_integer/0-is_palindrome.c
+++ 0x08-palindrome_integer/0-is_palindrome.c
@@ -2,6 +2,40 @@
 #include <stdlib.h>
 #include "palindrome.h"
 
+
+/**
+ * get_tenth - get tenth
+ *
+ * @n: get max tenth of this number
+ * Return: tenth
+ */
+unsigned long int get_tenth(unsigned long n)
+{
+	unsigned long i;
+
+	for (i = 1; n / 10; i *= 10)
+		n /= 10;
+	return (i);
+}
+
+/**
+ * check_pali - check if int is palidrome or not
+ *
+ * @n: number checking
+ * @i: starting point
+ * @tenth: end point
+ * Return: 1 if true, 0 if false
+ */
+int check_pali(unsigned long n, unsigned long i, unsigned long tenth)
+{
+	if ((n / i % 10 == n / tenth % 10) && (i < tenth))
+		return (check_pali(n, i * 10, tenth / 10));
+	else if (n / i % 10 != n / tenth % 10)
+		return (0);
+	else
+		return (1);
+}
+
 /**
  * is_palindrome - checks if unsigned int is palindrome
  * @n: unsigned int of given number to be checked
@@ -9,7 +43,5 @@
  */
 int is_palindrome(unsigned long n)
 {
-	if (!n)
-		return (0);
-	return (1);
+	return (check_pali(n, 1, get_tenth(n)));
 }
