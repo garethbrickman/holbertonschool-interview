4cfd03defa8301bb4fd53fae85f688a921ab4842
Gareth Brickman <977@holbertonschool.com>
2020-07-28 01:36:54 +0000

    Incl if name == main wrapper

--- 0x0C-nqueens/0-nqueens.py
+++ 0x0C-nqueens/0-nqueens.py
@@ -7,17 +7,18 @@ import sys
 
 # error handling for argv[1]
 
-if len(sys.argv) == 1 or len(sys.argv) > 2:
-    print("Usage: nqueens N")
-    sys.exit(1)
-N = sys.argv[1]
-try:
-    N_int = int(N)
-except ValueError:
-    print("N must be a number")
-    sys.exit(1)
-if N_int < 4:
-    print("N must be at least 4")
-    sys.exit(1)
+if __name__ == "__main__":
+    if len(sys.argv) == 1 or len(sys.argv) > 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+    N = sys.argv[1]
+    try:
+        N_int = int(N)
+    except ValueError:
+        print("N must be a number")
+        sys.exit(1)
+    if N_int < 4:
+        print("N must be at least 4")
+        sys.exit(1)
 
 # nqueens method
