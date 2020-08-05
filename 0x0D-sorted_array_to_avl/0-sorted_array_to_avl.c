7697f8f5b613187c0e32cf6facb4596d44252990
Gareth Brickman <977@holbertonschool.com>
2020-08-05 17:52:47 +0000

    Init commit

---
+++ 0x0D-sorted_array_to_avl/0-sorted_array_to_avl.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "binary_trees.h"
+
+
+/**
+ * sorted_array_to_avl - builds an AVL tree from array
+ *
+ * @array: ptr to first element of array
+ * @size: num of elements in array
+ *
+ * Return: ptr to root node of AVL tree, or NULL
+ */
+avl_t *sorted_array_to_avl(int *array, size_t size)
+{
+	if (!array || !size)
+		return(NULL);
+	return(0);
+}
---
+++ 0x0D-sorted_array_to_avl/README.md
@@ -0,0 +1,1 @@
+This is a README for my 0x0D-sorted_array_to_avl project!
