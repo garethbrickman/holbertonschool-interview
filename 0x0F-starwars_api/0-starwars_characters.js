b73e988602294ecd35a0286e3ee78b38d696cddb
Gareth Brickman <977@holbertonschool.com>
2020-08-17 03:08:27 +0000

    Fix code so char names are printed in URL order using async/await

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -3,23 +3,26 @@
 const request = require('request');
 
 const filmNum = process.argv[2] + '/';
-const filmUrl = 'https://swapi-api.hbtn.io/api/films/';
+const filmURL = 'https://swapi-api.hbtn.io/api/films/';
 
-request(filmUrl + filmNum, function (err, res, body) {
-  if (err) {
-    return console.error(err);
-  }
+// Makes API request, sets async to allow await promise
+request(filmURL + filmNum, async function (err, res, body) {
+  if (err) return console.error(err);
 
-  const characterList = JSON.parse(body).characters;
+  // find URLs of each character in the film as a list obj
+  const charURLList = JSON.parse(body).characters;
 
-  characterList.forEach(function (characterUrl) {
-    request(characterUrl, function (err, res, body) {
-      if (err) {
-        return console.error(err);
-      }
+  // Use URL list to character pages to make new requests
+  // await queues requests until they resolve in order
+  for (const charURL of charURLList) {
+    await new Promise(function (resolve, reject) {
+      request(charURL, function (err, res, body) {
+        if (err) return console.error(err);
 
-      const characterName = JSON.parse(body).name;
-      console.log(characterName);
+        // finds each character name and prints in URL order
+        console.log(JSON.parse(body).name);
+        resolve();
+      });
     });
-  });
+  }
 });
