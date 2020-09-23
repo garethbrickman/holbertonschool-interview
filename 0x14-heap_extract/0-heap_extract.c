e8c064d2c5c4923db81a0d13ffb5a56f4137751a
Gareth Brickman <977@holbertonschool.com>
2020-09-23 19:39:27 +0000

    Init commit

---
+++ 0x14-heap_extract/0-heap_extract.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "binary_trees.h"
+
+/**
+ * heap_extract - extracts root node of Max Binary Heap
+ *
+ * @root: double ptr to root node of heap
+ *
+ * Return: 0 on fail
+ */
+int heap_extract(heap_t **root)
+{
+	if (!*root)
+		return (0);
+}
---
+++ 0x14-heap_extract/binary_trees.h
@@ -0,0 +1,26 @@
+#ifndef _BINARY_TREES_H_
+#define _BINARY_TREES_H_
+
+#include <stddef.h>
+
+/**
+ * struct binary_tree_s - Binary tree node
+ *
+ * @n: Integer stored in the node
+ * @parent: Pointer to the parent node
+ * @left: Pointer to the left child node
+ * @right: Pointer to the right child node
+ */
+struct binary_tree_s
+{
+	int n;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
+}
+typedef struct binary_tree_s binary_tree_t;
+
+int heap_extract(heap_t **root);
+void binary_tree_print(const binary_tree_t *);
+
+#endif /* _BINARY_TREES_H_ */
