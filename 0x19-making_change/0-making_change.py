79df6af3860af3d9dc4f1dd1640e090c2d9b4328
Gareth Brickman <977@holbertonschool.com>
2020-11-09 16:48:29 +0000

    Add conditional to handle unsolveable case

--- 0x19-making_change/0-making_change.py
+++ 0x19-making_change/0-making_change.py
@@ -13,10 +13,12 @@ def makeChange(coins, total):
 
         coins = sorted(coins, reverse=True)
         coin_dict = {}
-        for c in coins:
-            if total % c == 0:
-                coin_dict[c] = total / c
-                return(int(sum(coin_dict.values())))
-            else:
-                coin_dict[c] = trunc(total / float(c))
-                total -= (c * coin_dict[c])
+        while total is not None:
+            for c in coins:
+                if total % c == 0:
+                    coin_dict[c] = total / c
+                    return(int(sum(coin_dict.values())))
+                else:
+                    coin_dict[c] = trunc(total / float(c))
+                    total -= (c * coin_dict[c])
+            return -1
