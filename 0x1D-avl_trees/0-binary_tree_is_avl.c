51850eafda6884384568276b9c90a3af3d69e1d7
Gareth Brickman <977@holbertonschool.com>
2020-12-07 05:04:05 +0000

    Init commit

---
+++ 0x1D-avl_trees/0-binary_tree_is_avl.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "binary_trees.h"
+
+
+/**
+ * binary_tree_is_avl - checks if binary tree is valid AVL tree
+ *
+ * @tree: ptr to root node
+ *
+ * Return: 1 if tree valid AVL, else 0
+ */
+int binary_tree_is_avl(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+	return (1);
+}
