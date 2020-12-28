cff7648b3bb5b17c7a88f1c3c8ee0495a16b84a8
Gareth Brickman <977@holbertonschool.com>
2020-12-28 17:23:48 +0000

    Complete code

--- 0x1F-pascal_triangle/0-pascal_triangle.py
+++ 0x1F-pascal_triangle/0-pascal_triangle.py
@@ -5,8 +5,25 @@
 def pascal_triangle(n):
     """ Returns list of lists of ints representing Pascal’s triangle of n """
 
-    new_list = []
+    if n <= 0:
+        return []
 
-    if len(n) <= 0:
-        return new_list
-        
+    pascal = [[1]]
+    if n == 1:
+        return pascal
+
+    for i in range(1, n):
+        left = -1
+        right = 0
+        in_pas = []
+        for j in range(i+1):
+            num = 0
+            if left > -1:
+                num += pascal[i - 1][left]
+            if right < i:
+                num += pascal[i - 1][right]
+            left += 1
+            right += 1
+            in_pas.append(num)
+        pascal.append(in_pas)
+    return pascal
